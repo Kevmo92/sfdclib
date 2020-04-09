223ce0ffe2e78be8a679b6cf07f333c1f2df4476
Kevin Watson <kevin.watson@ncino.com>
2020-04-09 18:54:52 -0400

    🐛 FIX: I cant count

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='sfdclib',
-    version='0.2.22',
+    version='0.2.27',
     author='Andrey Shevtsov & KevOps',
     author_email='ah.you@thought.com',
     packages=['sfdclib'],
